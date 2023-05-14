53a95a715cef0fd804c24198ffbecf7523a46a4d
adejoogaitnas <adejoogaitnas@gmail.com>
2023-05-14 23:04:31 +0200

    Primer commit

---
+++ programa1.py
@@ -0,0 +1,10 @@
+a = int(input("Ingrese el primer número:\n"))
+b = int(input("Ingrese el segundo número:\n"))
+
+
+def suma(a, b):
+    return f'El resultado de la suma es {a + b}'
+
+
+def resta(a, b):
+    return f'El resultado de la resta es {a - b}'
---
+++ programa2.py
@@ -0,0 +1,4 @@
+from programa1 import suma,resta,a,b
+
+print(suma(a, b))
+print(resta(a, b))
